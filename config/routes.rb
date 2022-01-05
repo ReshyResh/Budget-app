e392497ac59c1cc52f3983245c4f77a1f8c2bcc4
Reshy <reshy@protonmail.com>
2022-01-06 00:22:49 +0100

    Changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'categories#index'
+  get '/unauthorized' => 'categories#unauthorized'
+  get '/splash' => 'users#index'
   devise_scope :user do
     get '/sign-in' => "devise/sessions#new", :as => :login
   end
+  resources :users, only: [:index]
   resources :categories, only: [:index, :new, :show, :create, :destroy] do
-    resources :transactions, only: [:create, :destroy]
+    resources :transactions, only: [:index, :create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
